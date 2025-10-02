e92ddc3e146e01983462d02593b313d5d35d53bd
Muhammed Bilal M N <139211697+muhammedbilalmn@users.noreply.github.com>
2025-10-02 20:09:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+
+// document.getElementById('demo').textContent="hello again";
+// alert("hacked");
+// confirm("yes or no");
+// prompt("Enter your name");
+
+function show(){
+    console.log("show function");
+}
+show();
+const display=() =>{
+    console.log("show display");
+
+}
+display();
+
+function shw(name){
+    console.log("hai", name);
+}
+shw("bilal");
+
+const intro=(name1,name2)=>{
+    console.log("hai",name1,name2);
+}
+intro("bilal","Muthu");
+
+// Arrays
+
+let arr =[1,2,3,4,"hello"]
+console.log(arr);
+
+let details =[
+    {nam:"santhoor",mrp:123,country:"india"},
+    {nam:"dathri",mrp:100,country:"india"},
+    {nam:"Adidas",mrp:109899,country:"usa"}
+
+];
+console.log(details);
